aa5ae29bbc69afd5627d8561fd193bc2932bed8f
chenbangyu <chenbangyu@example.com>
2026-05-14 11:51:05 +0800

    Stop tracking simx build outputs

--- sim/simx/obj/common/util.d
+++
@@ -1,6 +0,0 @@
-/mnt/d/wode_code_trunk/vortex/sim/simx/obj/common/util.o: \
- /mnt/d/wode_code_trunk/vortex/sim/common/util.cpp \
- /mnt/d/wode_code_trunk/vortex/sim/common/util.h \
- /mnt/d/wode_code_trunk/vortex/sim/common/bitmanip.h
-/mnt/d/wode_code_trunk/vortex/sim/common/util.h:
-/mnt/d/wode_code_trunk/vortex/sim/common/bitmanip.h:
--- sim/simx/obj/tensor/open_tensorcore/local_memory/tensor_local_mem_arbiter.d
+++
@@ -1,4 +0,0 @@
-/mnt/d/wode_code_trunk/vortex/sim/simx/obj/tensor/open_tensorcore/local_memory/tensor_local_mem_arbiter.o: \
- /mnt/d/wode_code_trunk/vortex/sim/simx/tensor/open_tensorcore/local_memory/tensor_local_mem_arbiter.cpp \
- /mnt/d/wode_code_trunk/vortex/sim/simx/tensor/open_tensorcore/local_memory/tensor_local_mem_arbiter.h
-/mnt/d/wode_code_trunk/vortex/sim/simx/tensor/open_tensorcore/local_memory/tensor_local_mem_arbiter.h:
